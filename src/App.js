8bbc7e557bd0b3236e38013152c9063225ab2a34
Kemal Ongun <kemalabdullahongun@gmail.com>
2022-10-27 16:18:22 +0300

    todo uygulaması

--- src/App.js
+++ src/App.js
@@ -1,23 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import { Button, FormControl, FormCheck } from "react-bootstrap";
+import {ReactComponent as DeleteIcon} from "./assets/delete.svg"
+import {ReactComponent as EdıtIcon} from "./assets/edıt.svg";
+import {ReactComponent as SaveIcon} from "./assets/save.svg";
 
-function App() {
+ function App() {
+  const [newTodo, setNewTodo] = useState("");
+  const [todolist, setTodolist] = useState([]);
+
+  const handleClick = () => {
+    setTodolist(prevTodolist => [...prevTodolist, newTodo])
+    setNewTodo("");
+  }
+
+
+  // inputun içine girilen her değişikliği consala basar. didMount(). 
+  // useEffect(() => {
+  //   console.log(newTodo)
+  // },[newTodo]);
+
+  const taskInput = (e) => {
+    setNewTodo(e.target.value);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="d-flex flex-column justify-content-center align-items-center mt-5">
+      <h1 className="mt-5">Todo List</h1>
+
+      <div className="d-flex w-75 justify-content-space mt-3">
+        <FormControl
+          className="w-75"
+          placeholder="Writa a todo"
+          value={newTodo}
+          onChange={taskInput}
+        />
+        <Button onClick={handleClick} className="ms-5">
+          Add todo
+        </Button>
+      </div>
+      <div className="mt-5 w-75">
+        {
+          todolist.map((ıtem, i)=> (
+            
+            <div key={i} className="d-flex justify-content-between">
+              <div className="d-flex">
+                <FormCheck
+                type="checkbox"
+                className="me-2"
+                />
+                <label>
+                  { ıtem }
+                </label>
+              </div>
+              <div>
+                <DeleteIcon width={25} height={25} style={{cursor: 'pointer'}} className="me-2" />
+                <EdıtIcon width={25} height={25}  style={{cursor: 'pointer'}} className="me-2" />
+                <SaveIcon width={25} height={25}  style={{cursor: 'pointer'}} />
+              </div>
+            </div>
+             
+          ))
+        }
+      </div>
     </div>
   );
 }
